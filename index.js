bef9555c9fa4c1f29d79100d010debdfaa69b444
jrmkim50 <jrmkim50@gmail.com>
2022-07-26 19:18:34 -0400

    added a root endpoint

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const fetch = require('node-fetch');
 const app = express();
 app.use(cors());
 
+app.get('/', (req, res) => {
+  res.send("This server proxies requests, so there is no need to look into cors.");
+})
+
 app.get('/getFile', async (req, res) => {
   const { fileurl } = req.query;
   fetch(fileurl).then(response => response.buffer()).then(buff => {
